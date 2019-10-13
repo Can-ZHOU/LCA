fdbdc8053c67a85782db3aea91d54546fca72412
ZC <zhoucan1999@outlook.com>
2019-10-13 10:51:11 +0100

    Update test file with new wrong binary tree case and finishing format as well

--- src/LCA_binary_tree_Test.java
+++ src/LCA_binary_tree_Test.java
@@ -4,23 +4,6 @@ import org.junit.Test;
 
 public class LCA_binary_tree_Test {
 	
-//~ Node class-------------------------------------------------------------------------------------
-	
-	/**
-	 * Testing the node class.
-	 */
-	@Test
-	public void testNode() {
-		Node node = new Node(1);
-		assertEquals("Node(1)-data", 1, node.data);
-		assertNull("Node(1)-left_node", node.left);
-		assertNull("Node(1)-right_node", node.right);
-	}
-	
-	
-	
-	
-
 //~ LCA_binary_tree class--------------------------------------------------------------------------
 	
 	//~ Querying a node that does not exist in the binary tree-------------------------------------
@@ -33,6 +16,24 @@ public class LCA_binary_tree_Test {
 		assertEquals("Empty binary tree", -1, tree.findLCA(1, 2));
 	}		
 
+	/**
+	 * Testing the binary tree which is no answer for two nodes' LCA. It is expected to return -1.
+	 * 
+	 * ****** Visualization ******
+	 *             1
+	 *            / \
+	 *          2     3
+	 *         / \   / \
+	 *        4   5 6   7
+	 * ***************************
+	 */
+	@Test
+	public void testNoAnswer() {
+		LCA_binary_tree tree = new LCA_binary_tree(); 
+		tree.root = new Node(1);
+		assertEquals("There is no answer.", -1, tree.findLCA(10, 11));
+	}
+	
 	/**
 	 * Testing the binary tree which only has one node. It is expected to return -1.
 	 * 
@@ -64,7 +65,7 @@ public class LCA_binary_tree_Test {
 	 * ***************************
 	 */
 	@Test
-	public void testCommon() {
+	public void testCommon_case_one() {
 		LCA_binary_tree tree = new LCA_binary_tree(); 
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
@@ -74,11 +75,11 @@ public class LCA_binary_tree_Test {
 		tree.root.right.left = new Node(6);
 		tree.root.right.right = new Node(7);
 		
-		assertEquals("Fail when their parents (upper level) are their ancestors.", 2, tree.findLCA(4, 5));
-		assertEquals("Fail When one of the nodes is their common ancestor", 2, tree.findLCA(4, 2));
-		assertEquals("Fail When their common ancestors before the previous level_case one", 1, tree.findLCA(4, 6));
-		assertEquals("Fail When their common ancestors before the previous level_case two", 1, tree.findLCA(4, 3));
-		assertEquals("Fail When their common ancestor is themselves", 4, tree.findLCA(4, 4));
+		assertEquals("When their parents (upper level) are their ancestors.", 2, tree.findLCA(4, 5));
+		assertEquals("When one of the nodes is their common ancestor", 2, tree.findLCA(4, 2));
+		assertEquals("When their common ancestors before the previous level_case one", 1, tree.findLCA(4, 6));
+		assertEquals("When their common ancestors before the previous level_case two", 1, tree.findLCA(4, 3));
+		assertEquals("When their common ancestor is themselves", 4, tree.findLCA(4, 4));
 
 	}
 	
@@ -107,11 +108,11 @@ public class LCA_binary_tree_Test {
 		tree.root.left.left.right = new Node(7);
 		tree.root.left.right.right = new Node(8);
 		
-		assertEquals("Fail when their parents (upper level) are their ancestors.", 2, tree.findLCA(4, 5));
-		assertEquals("Fail When one of the nodes is their common ancestor", 2, tree.findLCA(4, 2));
-		assertEquals("Fail When their common ancestors before the previous level_case one", 1, tree.findLCA(3, 6));
-		assertEquals("Fail When their common ancestors before the previous level_case two", 2, tree.findLCA(6, 8));
-		assertEquals("Fail When their common ancestor is themselves", 4, tree.findLCA(4, 4));
+		assertEquals("When their parents (upper level) are their ancestors.", 2, tree.findLCA(4, 5));
+		assertEquals("When one of the nodes is their common ancestor", 2, tree.findLCA(4, 2));
+		assertEquals("When their common ancestors before the previous level_case one", 1, tree.findLCA(3, 6));
+		assertEquals("When their common ancestors before the previous level_case two", 2, tree.findLCA(6, 8));
+		assertEquals("When their common ancestor is themselves", 4, tree.findLCA(4, 4));
 
 	}
 	
@@ -136,16 +137,57 @@ public class LCA_binary_tree_Test {
 		tree.root.right.right = new Node(3);
 		tree.root.right.right.right = new Node(4);
 		
-		assertEquals("Fail When one of the nodes is their common ancestor_case one", 2, tree.findLCA(4, 2));
-		assertEquals("Fail When one of the nodes is their common ancestor_case two", 3, tree.findLCA(4, 3));
-		assertEquals("Fail When their common ancestor is themselves", 4, tree.findLCA(4, 4));
+		assertEquals("When one of the nodes is their common ancestor_case one", 2, tree.findLCA(4, 2));
+		assertEquals("When one of the nodes is their common ancestor_case two", 3, tree.findLCA(4, 3));
+		assertEquals("When their common ancestor is themselves", 4, tree.findLCA(4, 4));
 
 	}
 	
 	
 	//~ Querying the wrong binary tree-------------------------------------------------------------
 	/**
-	 * Testing the wrong binary tree -- case one.
+	 * Testing the the wrong binary tree -- a tree with duplicated nodes.
+	 * 
+	 * !!!!!!BUG CANNOT FIX!!!!!!!
+	 * There has duplicated node
+	 * -- 2 in tree.
+	 * so, for instance, LCA(6,2) 
+	 * should has two answers 1 or 3.
+	 * Therefore, it should not
+	 * pass all the test.
+	 * !!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 * 
+	 * ****** Visualization ******
+	 *             1
+	 *            / \
+	 *          2     3
+	 *         / \   / \
+	 *        4   5 6   2
+	 * ***************************
+	 */
+	@Test
+	public void testWrong_tree_duplicated_nodes() {
+		LCA_binary_tree tree = new LCA_binary_tree(); 
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(2);
+		
+		assertEquals("When their parents (upper level) are their ancestors.", 2, tree.findLCA(4, 5));
+		assertEquals("When one of the nodes is their common ancestor", 3, tree.findLCA(3, 6));
+		assertEquals("When their common ancestors before the previous level_case one", 1, tree.findLCA(4, 6));
+		assertEquals("When their common ancestors before the previous level_case two", 1, tree.findLCA(4, 3));
+		assertEquals("When their common ancestor is themselves", 4, tree.findLCA(4, 4));
+		
+		//This test cannot be passed, which should has two answers 1 or 3.
+		assertEquals("When their common ancestor is themselves", -1, tree.findLCA(6, 2));
+	}
+	
+	/**
+	 * Testing the wrong binary tree -- a tree with node has two parents.
 	 * 
 	 * !!!!!!BUG CANNOT FIX!!!!!!!
 	 * The node 5 has two parents
@@ -166,7 +208,7 @@ public class LCA_binary_tree_Test {
 	 * ***************************
 	 */
 	@Test
-	public void testWrong_case_one() {
+	public void testWrong_tree_two_parents() {
 		LCA_binary_tree tree = new LCA_binary_tree(); 
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
@@ -178,19 +220,18 @@ public class LCA_binary_tree_Test {
 		tree.root.left.left.left = new Node(7);
 		tree.root.left.left.right = new Node(8);
 		
-		assertEquals("Fail when their parents (upper level) are their ancestors.", 4, tree.findLCA(7, 8));
-		assertEquals("Fail When one of the nodes is their common ancestor", 4, tree.findLCA(4, 7));
-		assertEquals("Fail When their common ancestors before the previous level_case one", 2, tree.findLCA(7, 5));
-		assertEquals("Fail When their common ancestors before the previous level_case two", 1, tree.findLCA(7, 3));
-		assertEquals("Fail When their common ancestor is themselves", 4, tree.findLCA(4, 4));
-		assertEquals("Fail When one node has two parents_answer one", 2, tree.findLCA(4, 5));
+		assertEquals("When their parents (upper level) are their ancestors.", 4, tree.findLCA(7, 8));
+		assertEquals("When one of the nodes is their common ancestor", 4, tree.findLCA(4, 7));
+		assertEquals("When their common ancestors before the previous level_case one", 2, tree.findLCA(7, 5));
+		assertEquals("When their common ancestors before the previous level_case two", 1, tree.findLCA(7, 3));
+		assertEquals("When their common ancestor is themselves", 4, tree.findLCA(4, 4));
 		
-		//This test cannot be passed.
-		assertEquals("Fail When one node has two parents_answer two", 1, tree.findLCA(4, 5));
+		//This test cannot be passed, which should has two answers 1 or 2.
+		assertEquals("When one node has two parents_answer two", -1, tree.findLCA(4, 5));
 	}
 	
 	/**
-	 * Testing the wrong binary tree -- case two.
+	 * Testing the wrong binary tree -- a tree which has a loop.
 	 * 
 	 * !!!!!!BUG CANNOT FIX!!!!!!!
 	 * This test should have not
@@ -204,12 +245,32 @@ public class LCA_binary_tree_Test {
 	 * ***************************
 	 */
 	@Test
-	public void testWrong_case_two() {
+	public void testWrong_tree_has_loop() {
 		LCA_binary_tree tree = new LCA_binary_tree(); 
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
 		tree.root.right = new Node(3);
 		tree.root.left.right = tree.root.right;
+		tree.root.right.left = tree.root.left;
+		
+		//This test cannot be passed.
 		assertEquals("The test result should fail.", -1, tree.findLCA(2, 3));
+	}
+	
+
+	
+	
+	
+//~ Node class-------------------------------------------------------------------------------------
+	
+	/**
+	 * Testing the node class.
+	 */
+	@Test
+	public void testNode() {
+		Node node = new Node(1);
+		assertEquals("Node(1)-data", 1, node.data);
+		assertNull("Node(1)-left_node", node.left);
+		assertNull("Node(1)-right_node", node.right);
 	}
 }
