62496666ca6585615e35eebd6ea93bd9682e559f
ZC <zhoucan1999@outlook.com>
2019-10-14 21:55:31 +0100

    Update the DAG test case, the origianl solution cannot past test.

--- src/LCA_DAG_Test.java
+++ src/LCA_DAG_Test.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 public class LCA_DAG_Test {
 
+//~ LCA_DAG class----------------------------------------------------------------------------------
+	
+	//~ Querying a node that does not exist in the graph-------------------------------------------
+	/**
+	 * Testing the empty DAG. It is expected to return -1.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty() {
+		LCA_DAG graph = new LCA_DAG(0);
+		assertEquals("Empty DAG", -1, graph.findLCA(0, 0));
 	}
-
 }
